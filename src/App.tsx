60a1b5cae6489b5e7a3273dc766ca876f2def043
Alexander Georgiev <alexfromsofia@gmail.com>
2020-09-15 23:27:15 +0300

    Add simple API call to trivia and ref usage for focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import { useFetch } from "./useFetch";
+import { useForm } from "./useForm";
+
+interface IValues {
+  email: "";
+  password: "";
+  firstName: "";
+}
 
 function App() {
+  const [values, handleChange] = useForm<IValues>({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+  const [count, setCount] = useState(() =>
+    JSON.parse((window.localStorage.getItem("count") as any) || 0)
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{!data || loading ? "loading..." : data}</div>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount((c: number) => c + 1)}>Increment</button>
+      <>
+        <input
+          ref={inputRef}
+          placeholder="Email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="First Name"
+          name="firstName"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Password"
+          name="password"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <button onClick={() => (inputRef as any).current.focus()}>Focus</button>
+      </>
     </div>
   );
 }
